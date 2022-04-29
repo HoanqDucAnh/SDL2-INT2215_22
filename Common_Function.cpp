0cbbab35fe35af8aa38bb0e37c0ddef578a7ba8d
HoanqDucAnh <100891275+HoanqDucAnh@users.noreply.github.com>
2022-04-29 11:14:26 +0700

    fix commonFunc namespace

--- Common_Function.cpp
+++ Common_Function.cpp
@@ -1,7 +1,7 @@
 #include "Common_Function.h"
 
 
-bool CheckCollision(const SDL_Rect& object1, const SDL_Rect& object2)
+bool SDLCommonFunction::CheckCollision(const SDL_Rect& object1, const SDL_Rect& object2)
 {
     int left_a = object1.x;
     int right_a = object1.x + object1.w;
